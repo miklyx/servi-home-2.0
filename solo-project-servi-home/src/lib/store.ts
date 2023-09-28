f754fc6b22e14629128a5cbdf06abfc31def5a5b
Mikhail Rashkovskiy <miklyx@gmail.com>
2023-09-28 20:39:53 +0200

    fix: cleanerlogin.ts

--- solo-project-servi-home/src/lib/store.ts
+++ solo-project-servi-home/src/lib/store.ts
@@ -2,11 +2,11 @@ import { create } from 'zustand'
 
 interface Auth {
   auth: string,
-  setAuth: (auth: string) => string,
-  removeAuth: () => string
+  setAuth: (auth: string) => void,
+  removeAuth: () => void
 }
 
-export const useAuth = create((set) => ({
+export const useAuth = create<Auth>((set) => ({
   auth: null,
   setAuth: (auth) => set(() => ({ auth })),
   removeAuth: () => set({ auth: null }),
