8aebbe5efae80bd06cce4213cbfc55af5a921326
AntkiewiczGrzegorz1997 <gantkiewicz97@gmail.com>
2023-09-29 11:52:35 +0200

    fix: api/updateBookingStatus q
    q:
    :q
    :wq

--- solo-project-servi-home/src/pages/api/updateBookingStatus.ts
+++ solo-project-servi-home/src/pages/api/updateBookingStatus.ts
@@ -1,14 +1,22 @@
-import { PrismaClient } from "@prisma/client";
-import { sendEmail } from "../../lib/sendEmail";
-import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from '@prisma/client';
+// import { sendEmail } from '../../lib/sendEmail';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 const prisma = new PrismaClient();
-const sendEmail = async function (email:string, title:string, text:string) : Promise<void> {
-  await sendEmail(email, 'Service Confirmation', 'A cleaner has accepted your service!'); 
-}
+const sendEmail = async function (
+  email: string,
+  title: string,
+  text: string
+): Promise<void> {
+  await sendEmail(
+    email,
+    'Service Confirmation',
+    'A cleaner has accepted your service!'
+  );
+};
 
-export default async (req:NextApiRequest, res:NextApiResponse) => {
-  if (req.method !== "POST") {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
     return res.status(405).end();
   }
 
@@ -26,15 +34,19 @@ export default async (req:NextApiRequest, res:NextApiResponse) => {
         user: true,
       },
     });
-    
-  
-    await sendEmail(updatedBooking.user.email, 'Service Confirmation', 'A cleaner has accepted your service!');
-  
+
+    await sendEmail(
+      updatedBooking.user.email,
+      'Service Confirmation',
+      'A cleaner has accepted your service!'
+    );
 
     return res.status(200).json(updatedBooking);
-  } catch (error) {
-    console.error("Error updating booking:", error);
-    return res.status(500).json({ message: `Internal Server Error: ${error.message}` });
+  } catch (error: any) {
+    console.error('Error updating booking:', error);
+    return res
+      .status(500)
+      .json({ message: `Internal Server Error: ${error.message}` });
   } finally {
     await prisma.$disconnect();
   }
