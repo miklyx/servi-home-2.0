6176bf2ece64560c6a9ca1f0cc1f6fe0e388b486
AntkiewiczGrzegorz1997 <gantkiewicz97@gmail.com>
2023-09-29 10:51:34 +0200

    last

---
+++ solo-project-servi-home/src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import Layout from '../components/Layout';
+import 'src/styles/globals.css';
+import React, { useEffect } from 'react';
+import { useAuth } from '../lib/store';
+import { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    const auth: string | null = localStorage.getItem('auth');
+    if (auth) {
+      useAuth.getState().setAuth(JSON.parse(auth));
+    }
+  }, []);
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
