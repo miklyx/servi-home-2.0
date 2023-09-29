d82ef6a38d8eb58df0e944f8b3de7fd68ce9f8da
AntkiewiczGrzegorz1997 <gantkiewicz97@gmail.com>
2023-09-29 11:55:48 +0200

    fix: reviews

--- solo-project-servi-home/src/types.ts
+++ solo-project-servi-home/src/types.ts
@@ -22,24 +22,10 @@ export interface BookingData {
   status?: string;
 }
 
-interface Service {
+export interface Reviews {
   id: string;
-  type: string;
-  description: string;
-  imageUrl: string | null;
-  bookingId: string;
+  title: string;
+  name: string;
+  content: string;
+  createdAt: Date;
 }
-
-// export interface Booking {
-
-//     id: String;
-//     userId:     String;
-//     user:       User;
-//     cleaner:    Cleaner;      @relation(fields: [cleanerId], references: [id])
-//     addressId:  String;
-//     address:    Address;        @relation(fields: [addressId], references: [id])
-//     services:   Service[]
-//     status     BookingStatus  @default(PENDING)
-//     createdAt  DateTime       @default(now())
-//   }
-// }
