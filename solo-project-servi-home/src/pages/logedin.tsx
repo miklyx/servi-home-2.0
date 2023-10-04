4ed094a6b4fd1eb3b0a94f27a4d2ea0cb899bcad
Mikhail Rashkovskiy <miklyx@gmail.com>
2023-10-04 13:18:44 +0200

    feat: created address autocomplete service but it does not work in frontend

--- solo-project-servi-home/src/pages/logedin.tsx
+++ solo-project-servi-home/src/pages/logedin.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { getDistrictsOfBerlin, getStreetsByIndex, getStreets, addressAutocomplete } from '../lib/addressService';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
 
 interface Service {
   title: string;
@@ -10,7 +13,13 @@ interface Service {
   material: string;
 }
 
+function classNames(...classes:any) {
+  return classes.filter(Boolean).join(' ')
+}
+
 function Logedin(): JSX.Element {
+  const districts = getDistrictsOfBerlin();
+
   const [services, setServices] = useState<Service[]>([]);
   const [rugmeasure, setRugmeasure] = useState<string>('');
   const [measure, setMeasure] = useState<string>('');
@@ -22,10 +31,35 @@ function Logedin(): JSX.Element {
 
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [modalMessage, setModalMessage] = useState<string>('');
+  const [selectedDistrict, setSelectedDistrict] = useState<string>(districts[0]);
+  const [index, setIndex] = useState<number>();
+  const [streets, setStreets] = useState<string[]>([]);
+  const [addressList, setAddressList] = useState<any[]>([]);
+
 
   const addService = (service: any) => {
     setServices([...services, service]);
   };
+
+  /* const StreetList = (currentIndex:number) => {
+    const currentStreets: any = getStreets(currentIndex)
+    return currentStreets
+  } */
+
+  const handleIndexChange = (e:any) => {
+    const newIndex = e.target.value;
+    setIndex(newIndex);
+    const currentStreets:any = getStreets(parseInt(newIndex))
+    setStreets(currentStreets);
+  };
+
+  const handleAddressChange = (e:any) => {
+    const newAddress = e.target.value;
+    setAddress(newAddress)
+    const newAddressList : any =  addressAutocomplete(newAddress);
+    setAddressList(newAddressList)
+
+  }
 
   const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -314,11 +348,108 @@ function Logedin(): JSX.Element {
                 type='text'
                 name='address'
                 value={address}
-                onChange={(e) => setAddress(e.target.value)}
+                onChange={handleAddressChange}/* {(e) => setAddress(e.target.value)} */
                 className=' border border-black bg-transparent rounded focus:outline-none focus:border-blue-400 m-2'
                 required
               />
+              {/* <label>Found  </label>
+                <input list="Berlin-addresses" name="Addresss"/>
+                {addressList && 
+                <datalist id="Berlin-addresses">
+                  {addressList.map((adr) => (
+                  <option key={adr.properties.name} value={adr.properties.name}></option>
+                ))}
+                </datalist>} */}
             </div>
+            {/*-------BEGIN---------EXPERIMENTAL -- DROPDOWN LIST */}
+            {/* <div>
+              <Listbox value={selectedDistrict} onChange={setSelectedDistrict}>
+                {({ open }) => (
+                  <>
+                  <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">Assigned to</Listbox.Label>
+                  <div className="relative mt-2">
+                  <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                    <span className="flex items-center">
+                      <span className="ml-3 block truncate">{selectedDistrict}</span>
+                    </span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                      <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                    </span>
+                  </Listbox.Button>
+                  
+                  <Transition
+                    show={open}
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                  
+                    <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                      {districts.map((district) => (
+                        <Listbox.Option 
+                            key={district} 
+                            value={district}
+                            className={({ active }) =>
+                              classNames(
+                                active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                                'relative cursor-default select-none py-2 pl-3 pr-9'
+                                )
+                              }
+                              >
+                              {({ selected, active }) => (
+                          <>
+                            <div className="flex items-center">
+                              <span
+                                className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
+                              >
+                                {district}
+                              </span>
+                            </div>
+
+                            {selected ? (
+                              <span
+                                className={classNames(
+                                  active ? 'text-white' : 'text-indigo-600',
+                                  'absolute inset-y-0 right-0 flex items-center pr-4'
+                                )}
+                              >
+                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                        </Listbox.Option>
+                      ))}
+                      </Listbox.Options>
+                  </Transition>
+                  </div>
+                </>
+              )}
+              </Listbox>
+            </div> */}
+            {/*------END-------EXPERIMENTAL -- DROPDOWN LIST */}
+
+            {/*------START-------EXPERIMENTAL -- HTML INPUT */}
+            {/* <div>
+                <label>District  </label>
+                <input list="Berlin-districts" name="District"/>
+                <datalist id="Berlin-districts">
+                  {districts.map((district) => (
+                  <option key={district} value={district}></option>
+                ))}
+                </datalist>
+                <label>Index  </label>
+                <input 
+                  type="text"
+                  value={index}
+                  onChange={handleIndexChange}>
+                </input>
+                <label>Street  </label>
+                {addressList[2]}
+            </div> */}
+            {/*------END-------EXPERIMENTAL -- HTML INPUT */}
+
             <p className='text-xs mt-4'>
               A cleaner proposal will be sent to you, as soon as posible you
               will obtain an email for confirmation
@@ -366,6 +497,7 @@ function Logedin(): JSX.Element {
         </div>
       )}
     </div>
+    
   );
 }
 
