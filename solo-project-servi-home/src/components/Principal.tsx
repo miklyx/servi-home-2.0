4df8158d2f7e32ba9505c13cf883cfdde9aa33af
ciisiq <cii_3@hotmail.com>
2024-01-10 15:42:13 +0000

    Style: Principal mobile style

--- solo-project-servi-home/src/components/Principal.tsx
+++ solo-project-servi-home/src/components/Principal.tsx
@@ -3,43 +3,51 @@ import style from '../styles/Cleaner.module.css';
 
 export default function Principal(): JSX.Element {
   return (
-    <div className='font-serif'>
-      <div className='bg-gradient-to-r from-zinc-500 to-zinc-600 flex-row p-12 justify-around items-center h-screen text-white flex'>
-        <div className='flex flex-col justify-center space-y-8'>
-          <p className='text-5xl leading-tight'>
+    <div className="text-center md:text-lefth-full font-serif">
+      <section
+        className={
+          'styleSection flex-col-reverse bg-gradient-to-r from-zinc-500 to-zinc-600 md:h-screen md:flex-row md:justify-around items-center '
+        }
+      >
+        <div className="flex flex-col justify-center space-y-8">
+          <h1 className="text-3xl text-center pt-8 md:pt-0  md:text-left md:text-4xl 2xl:text-6xl font-bold leading-tight">
             Transform your space <br />
             into a haven of cleanliness <br />
             and freshness.
-          </p>
-          <p className='text-3xl'>Your comfort, our priority.</p>
-          <button className='bg-yellow-500 text-2xl flex items-center justify-center text-black px-8 py-3 rounded-full transform transition-transform duration-300 hover:scale-105 hover:shadow-xl relative'>
-            <Link className='hover:underline' href='/signup'>
+          </h1>
+          <h2 className="text-2xl text-center md:text-left md:text-4xl">
+            Your comfort, our priority.
+          </h2>
+          <button className="bg-yellow-500 text-2xl flex items-center justify-center text-black md:px-8 py-3 rounded-full transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+            <Link className="hover:underline" href="/signup">
               Schedule your cleaner
             </Link>
           </button>
         </div>
-        <div className='rounded-full shadow-2xl hover:scale-105 transform transition-transform duration-300 border-4 border-yellow-500 p-1'>
+        <div className="rounded-full shadow-2xl hover:scale-105 transform transition-transform duration-300 border-4 border-yellow-500 md:p-1 md:w-[520px] 2xl:w-[750px]">
           <img
             className={`${style.image} ${style.customcursor}`}
-            src='/cleaning.png'
-            alt='Cleaning'
+            src="/cleaning.png"
+            alt="Cleaning"
           />
           <img
             className={`${style.upperimage} h-full`}
-            src='/cleanerhand.png'
-            alt='Cleaninghand'
+            src="/cleanerhand.png"
+            alt="Cleaninghand"
           />
         </div>
-      </div>
+      </section>
 
-      <div className='bg-zinc-400 p-12 flex justify-center text-white'>
-        <div className='text-4xl max-w-2xl text-center leading-relaxed font-medium relative'>
-          Don't miss the opportunity to elevate your living space to a new level
-          of cleanliness and comfort. Try our services today and experience the
-          difference for yourself!
-          <div className='absolute left-0 right-0 bottom-0 mx-auto h-0.5 bg-white w-3/4'></div>
+      <section className={'styleSection md:h-5/6 md:p-12 bg-zinc-400'}>
+        <div className="flex flex-col items-center justify-center ">
+          <p className="text-3xl  justify-center md:text-4xl 2xl:text-5xl max-w-6xl text-center leading-relaxed font-medium ">
+            Don't miss the opportunity to elevate your living space to a new
+            level of cleanliness and comfort. Try our services today and
+            experience the difference for yourself!
+          </p>
+          <div className=" mx-auto h-0.5 mt-10 bg-white w-3/4"></div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
